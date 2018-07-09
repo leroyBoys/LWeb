e8877a4b082ea5f65d64198ddf52969ece747efd
leroyBoys <wlvxiaohui@163.com>
2018-07-09 19:34:18 +0800

    33

--- DbBase/src/main/java/com/dbbase/moudle/admin/Admin.java
+++ DbBase/src/main/java/com/dbbase/moudle/admin/Admin.java
@@ -17,6 +17,14 @@ public class Admin {
     private String password;
     @LQField
     private String image;
+    @LQField
+    private String real_name;
+    @LQField
+    private String phone;
+    @LQField
+    private String id_card;
+    @LQField
+    private byte sex;
 
     public int getId() {
         return id;
@@ -28,6 +36,38 @@ public class Admin {
 
     public String getName() {
         return name;
+    }
+
+    public String getReal_name() {
+        return real_name;
+    }
+
+    public void setReal_name(String real_name) {
+        this.real_name = real_name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public byte getSex() {
+        return sex;
+    }
+
+    public void setSex(byte sex) {
+        this.sex = sex;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getId_card() {
+        return id_card;
+    }
+
+    public void setId_card(String id_card) {
+        this.id_card = id_card;
     }
 
     public void setName(String name) {
